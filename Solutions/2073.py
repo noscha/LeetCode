87be569b367eeced586d3a012315413062186961
noscha <93490208+noscha@users.noreply.github.com>
2024-04-09 13:13:44 +0200

    Update 2073.py

--- Solutions/2073.py
+++ Solutions/2073.py
@@ -3,9 +3,9 @@ class Solution:
         ''' If an entry larger or equal than tickets[k] it will appear tickets[k] times,
             if an entry is smaller, it will appear according to its amount '''
         res = 0
-        for i in tickets:
-            if i >= tickets[k]:
-                res += tickets[k]
+        for i in range(len(tickets)):
+            if tickets[i] >= tickets[k]:
+                res += tickets[k] - int(i > k)
             else:
-                res+= i
+                res+= tickets[i]
         return res
