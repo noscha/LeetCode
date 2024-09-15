65df31cc9e3498a730330ee1328c0032eedcfe70
noscha <93490208+noscha@users.noreply.github.com>
2024-09-15 12:45:44 +0000

    Update 1371.py

--- Solutions/1371.py
+++ Solutions/1371.py
@@ -3,13 +3,11 @@ class Solution:
         """ Bitmask encodes 0: even, 1: odd count of vowels"""
         
         bit_shift = {'a':4, 'e':3, 'i':2, 'o':1, 'u':0}
-        prev = 1 << bit_shift[s[0]] if s[0] in bit_shift else 0
         hm = {0: -1}
-        if prev != 0:
-            hm[prev] = 0
+        prev = 0
         res = 0
 
-        for i in range(1, len(s)):
+        for i in range(len(s)):
 
             mask = prev ^ 1 << bit_shift[s[i]] if s[i] in bit_shift else prev
 
