1c70a1e2af22afc111aef0ad1b87d1f6da8046ec
noscha <93490208+noscha@users.noreply.github.com>
2025-03-18 22:56:01 +0100

    Create 2401.py

---
+++ Solutions/2401.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        """
+        Sliding window check for overlapping bits
+        Bit mask need to stay zero under "and" operation, for no overlapping bits
+        "Or" adds to bit mask
+        "Xor" subtracts from bit mask
+        """
+
+        l, cur, res = 0, 0, 1
+        for r in range(len(nums)):
+            while cur & nums[r]:
+                cur ^= nums[l]
+                l += 1
+
+            cur |= nums[l]
+            res = max(res, r - l + 1)
+
+        return res
