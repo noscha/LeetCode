91266072ae952d4a645e2465759b3edcb0da5905
noscha <93490208+noscha@users.noreply.github.com>
2025-04-14 16:52:32 +0200

    Create 1534.py

---
+++ Solutions/1534.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        """
+        Brute Force, three loops
+        """
+
+        res = 0
+
+        for i in range(len(arr) - 2):
+            for j in range(i + 1,len(arr) - 1):
+                for k in range(j + 1, len(arr)):
+                    res += int(abs(arr[i] - arr[j]) <= a
+                        and abs(arr[j] - arr[k]) <= b
+                        and abs(arr[i] - arr[k]) <= c)
+
+        return res
